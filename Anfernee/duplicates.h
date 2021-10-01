59b7d5ffc684fbc9beeb0712a402041a19e778ff
anferneealviar99 <anferneealviar99@hotmail.com>
2021-10-01 10:30:31 +0800

    ok

---
+++ Anfernee/duplicates.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+// DECLARE GLOBAL FUNCTIONS
+extern char *strSHA2(char *filename);
+extern void read_directory(ch
+
+
---
+++ Anfernee/read_directory.c
@@ -0,0 +1,1 @@
+#include "duplicates.h"
