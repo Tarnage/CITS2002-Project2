584576985760e55e5436f498788727238cf25f10
Tarnage <70882644+Tarnage@users.noreply.github.com>
2021-10-03 16:17:47 +0800

    defined recursion still needs work

--- Tom/read_dir.c
+++ Tom/read_dir.c
@@ -11,8 +11,6 @@
 
 #include "duplicates.h"
 
-#define	OPTLIST		"r"
-
 #if     defined(__linux__)
 extern         char *strdup(const char *s);
 #endif
@@ -42,41 +40,103 @@ void scan_directory(char *dirname)
     }
 
 //  READ FROM THE REQUIRED DIRECTORY, UNTIL WE REACH ITS END
-    while((dp = readdir(dirp)) != NULL) {  
-        struct stat     stat_info;
-        char            pathname[MAXPATHLEN];
+    while((dp = readdir(dirp)) != NULL) {
+        if( (strcmp(dp->d_name, ".") != 0) && (strcmp(dp->d_name, "..") != 0) ){  
+            struct stat     stat_info;
+            char            pathname[MAXPATHLEN];
 
-//  SENDS FORMATTED STRING TO STRING POINTER POINTED BY pathname
-        sprintf(pathname, "%s/%s", dirname, dp->d_name);
-        // FOR TESTING
-        printf("%s/%s\n", dirname, dp->d_name);
+    //  SENDS FORMATTED STRING TO STRING POINTER POINTED BY pathname
+            sprintf(pathname, "%s/%s", dirname, dp->d_name);
+            // FOR TESTING
+            printf("%s/%s\n", dirname, dp->d_name);
 
-//  DETERMINE ATTRIBUTES OF THIS DIRECTORY ENTRY
-        if(stat(pathname, &stat_info) != 0) {
-            perror( pathname );
-            exit(EXIT_FAILURE);
+    //  DETERMINE ATTRIBUTES OF THIS DIRECTORY ENTRY
+            if(stat(pathname, &stat_info) != 0) {
+                perror( pathname );
+                exit(EXIT_FAILURE);
+            }
+
+    //  EXTEND OUR ARRAY OF STRUCTURES BY ONE ELEMENT
+            files                   = realloc(files, (nfiles+1)*sizeof(files[0]));
+            CHECK_ALLOC(files);			// ensure allocation was OK
+
+    //  REMEMBER (COPY) THIS ELEMENT'S RELATIVE PATHNAME
+            files[nfiles].pathname  = strdup(pathname);
+            CHECK_ALLOC(files[nfiles].pathname);	// ensure allocation was OK
+
+    //  REMEMBER THIS ELEMENT'S MODIFICATION TIME
+            files[nfiles].mtime     = stat_info.st_mtime;     // TODO maybe wont need this
+            files[nfiles].bytesize  = stat_info.st_size;      // its byte size
+            nbytes                 += stat_info.st_size;      // add to total bytes so far
+            ++nfiles;
         }
-
-//  EXTEND OUR ARRAY OF STRUCTURES BY ONE ELEMENT
-        files                   = realloc(files, (nfiles+1)*sizeof(files[0]));
-	    CHECK_ALLOC(files);			// ensure allocation was OK
-
-//  REMEMBER (COPY) THIS ELEMENT'S RELATIVE PATHNAME
-        files[nfiles].pathname  = strdup(pathname);
-	    CHECK_ALLOC(files[nfiles].pathname);	// ensure allocation was OK
-
-//  REMEMBER THIS ELEMENT'S MODIFICATION TIME
-        files[nfiles].mtime     = stat_info.st_mtime;     // TODO maybe wont need this
-        files[nfiles].bytesize  = stat_info.st_size;      // its byte size
-        nbytes                 += stat_info.st_size;      // add to total bytes so far
-        ++nfiles;
     }
-
-//  TODO ADD CHECK FOR UNIQUE FILES AND COUNT THE UNIQUE FILE. (THE USE OF HASH TABLE MIGHT BE THE SOLUTION)
-
 //  CLOSE THE DIRECTORY
     closedir(dirp);
 }
+
+//  SCANS DIRECTORY RECURSIVELY 
+void scan_dir_recur(char *dirname)
+{
+    DIR             *dirp;
+    struct dirent   *dp;
+
+//  ENSURE THAT WE CAN OPEN (read-only) THE REQUIRED DIRECTORY
+    dirp       = opendir(dirname);
+    if(dirp == NULL) {
+        perror( dirname );
+        exit(EXIT_FAILURE);
+    }
+
+//  READ FROM THE REQUIRED DIRECTORY, UNTIL WE REACH ITS END
+    while((dp = readdir(dirp)) != NULL) {
+
+        struct stat     stat_info;
+        char            pathname[MAXPATHLEN];
+
+        // if((strcmp(dp->d_name, ".") != 0) && (strcmp(dp->d_name, "..") != 0) ){
+        //     printf("%i:\n", stat_info.st_mode);
+        //     printf("%s:\n", dp->d_name);
+        // }
+
+        if( (strcmp(dp->d_name, ".") != 0) && (strcmp(dp->d_name, "..") != 0) ){  
+//  SENDS FORMATTED STRING TO STRING POINTER POINTED BY pathname
+            sprintf(pathname, "%s/%s", dirname, dp->d_name);
+            // FOR TESTING
+            printf("%s/%s\n", dirname, dp->d_name);
+
+//  DETERMINE ATTRIBUTES OF THIS DIRECTORY ENTRY
+            if(stat(pathname, &stat_info) != 0) {
+                perror( pathname );
+                exit(EXIT_FAILURE);
+            }
+
+//  EXTEND OUR ARRAY OF STRUCTURES BY ONE ELEMENT
+            files                   = realloc(files, (nfiles+1)*sizeof(files[0]));
+            CHECK_ALLOC(files);			// ensure allocation was OK
+
+//  REMEMBER (COPY) THIS ELEMENT'S RELATIVE PATHNAME
+            files[nfiles].pathname  = strdup(pathname);
+            CHECK_ALLOC(files[nfiles].pathname);	// ensure allocation was OK
+
+//  REMEMBER THIS ELEMENT'S MODIFICATION TIME
+            files[nfiles].mtime     = stat_info.st_mtime;     // TODO maybe wont need this
+            files[nfiles].bytesize  = stat_info.st_size;      // its byte size
+            nbytes                 += stat_info.st_size;      // add to total bytes so far
+            ++nfiles;
+        }
+
+//  CHECKS IF FILE IS A DIRECTORY AND RECURSIVELY READS FILES 
+        if(S_ISDIR(stat_info.st_mode) && 
+            strcmp(dp->d_name, ".") != 0 &&
+                strcmp(dp->d_name, "..") != 0){
+            scan_dir_recur(pathname);
+        }
+    }
+//  CLOSE THE DIRECTORY
+    closedir(dirp);
+}
+
 
 void print_dir_summary()
 {
